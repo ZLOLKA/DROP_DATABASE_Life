c677e8591d17d7170e1704ada9adbb2064124eb0
Zlolka <jaroslavderkach@gmail.com>
2021-05-06 10:37:19 +0300

    Ksyu: Added environment variables to settings

--- Ksyu/Ksyu/settings.py
+++ Ksyu/Ksyu/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,17 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", 0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "Main",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'Ksyu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE":   os.environ.get("SQL_ENGINE",   "django.db.backends.sqlite3"),
+        "NAME":     os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER":     os.environ.get("SQL_USER",     "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST":     os.environ.get("SQL_HOST",     "localhost"),
+        "PORT":     os.environ.get("SQL_PORT",     "5432"),
     }
 }
 
