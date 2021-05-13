136fa759b6e3530a36c77d027c095a828fb9b3d3
Zlolka <jaroslavderkach@gmail.com>
2021-05-13 18:46:44 +0300

    Ksyu: Add first page

--- Ksyu/Ksyu/settings.py
+++ Ksyu/Ksyu/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Ksyu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
